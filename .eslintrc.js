4b87950d291f133cdf6fe6aecd3ec46668767263
Bas950 <me@bas950.com>
2021-11-13 14:46:40 +0100

    feat: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,24 @@
 "use strict";
 
 module.exports = {
-  root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:eslint-plugin/recommended",
-    "plugin:node/recommended",
-  ],
-  env: {
-    node: true,
-  },
-  overrides: [
-    {
-      files: ["tests/**/*.js"],
-      env: { mocha: true },
-    },
-  ],
+	root: true,
+	extends: [
+		"eslint:recommended",
+		"plugin:eslint-plugin/recommended",
+		"plugin:node/recommended",
+		"plugin:prettier/recommended"
+	],
+	env: {
+		node: true
+	},
+	rules: {
+		"prettier/prettier": "error"
+	},
+	overrides: [
+		{
+			files: ["tests/**/*.js"],
+			env: { mocha: true }
+		}
+	],
+	plugins: ["prettier"]
 };
